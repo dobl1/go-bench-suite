4a5fc920735d69ceb3671604c8959a0d7d1d242f
Dorian <blanc.dorian.pro@gmail.com>
2024-03-26 21:20:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t mangomm/go-bench-suite:latest --no-cache --build-arg VERSION=master .
-docker push mangomm/go-bench-suite:latest
+docker build -t dobl1/go-bench-suite:latest --no-cache --build-arg VERSION=master .
+docker push dobl1/go-bench-suite:latest
